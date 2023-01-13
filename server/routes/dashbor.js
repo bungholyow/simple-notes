8b380340fe0b3518d804e3380a04e98332f1a14e
tommi <aritonang31031996@gmail.com>
2023-01-13 09:47:55 +0700

    homepage dan pagination fix

--- server/routes/dashbor.js
+++ server/routes/dashbor.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const dashborController = require("../controllers/dashborController");
+const { isLoggedIn } = require("../middleware/checkAuth");
 
 // dasbor route
-router.get("/dashbor", dashborController.dashbor);
+router.get("/dashbor", isLoggedIn, dashborController.dashbor);
+router.get("/dashbor/tambah", isLoggedIn, dashborController.dashboardAddNote);
+router.post(
+  "/dashbor/tambah",
+  isLoggedIn,
+  dashborController.dashboardAddNoteSubmit
+);
 
 module.exports = router;
